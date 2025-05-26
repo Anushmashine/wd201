9a41a31d37acf11c95d24d964ff02d7f5f9d4a81
Anushmashine <130737729+Anushmashine@users.noreply.github.com>
2025-05-26 23:49:19 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,22 +1,50 @@
 const express = require("express");
-const path = require("path");
+const app = express();
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const { Todo } = require("./models");
 
-const app = express();
-
-// Middleware
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Set EJS as view engine
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(csrf({ cookie: true }));
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 
-// Routes
 app.get("/", async (req, res) => {
   const todos = await Todo.findAll();
-  res.render("index", { todos });
+  res.render("index", {
+    overdue: todos.filter(t => t.isOverdue()),
+    dueToday: todos.filter(t => t.isDueToday()),
+    dueLater: todos.filter(t => t.isDueLater()),
+    completed: todos.filter(t => t.completed),
+    csrfToken: req.csrfToken(),
+  });
+});
+
+app.post("/todos", async (req, res) => {
+  const { title, dueDate } = req.body;
+  if (!title || !dueDate) {
+    return res.status(400).send("Title and dueDate are required");
+  }
+  await Todo.addTask({ title, dueDate });
+  res.redirect("/");
+});
+
+app.put("/todos/:id", async (req, res) => {
+  const todo = await Todo.findByPk(req.params.id);
+  if (todo) {
+    await todo.setCompletionStatus(req.body.completed);
+    res.json(todo);
+  } else {
+    res.status(404).send("Not found");
+  }
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  await Todo.remove(req.params.id);
+  res.status(200).send("Deleted");
 });
 
 module.exports = app;
