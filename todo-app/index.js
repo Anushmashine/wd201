b77cfd9b03f1b84fee8e63d5d243a07ae4b01cd9
Anushmashine <130737729+Anushmashine@users.noreply.github.com>
2025-05-26 23:34:30 +0530

    Update index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,34 +1,7 @@
-const express = require("express");
-const app = express();
-const csrf = require("csurf");
-const methodOverride = require("method-override");
-const bodyParser = require("body-parser");
-const { Todo } = require("./models");
+const app = require("./app");
 
-const csrfProtection = csrf({ cookie: false });
+const PORT = process.env.PORT || 3000;
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
-app.use(csrfProtection);
-
-app.get("/", async (req, res) => {
-  try {
-    const allTodos = {
-      overdue: await Todo.overdue(),
-      dueToday: await Todo.dueToday(),
-      dueLater: await Todo.dueLater(),
-      completed: await Todo.completed(),
-    };
-
-    res.render("index", {
-      allTodos,
-      csrfToken: req.csrfToken(),
-      today: new Date().toISOString().split("T")[0],
-    });
-  } catch (err) {
-    console.error("Error rendering index:", err);
-    res.status(500).send("Something went wrong.");
-  }
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
