50562baf2eeebce31db7ffc086cd75d57db21bb1
Anushmashine <130737729+Anushmashine@users.noreply.github.com>
2025-05-26 20:25:17 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,64 +1,68 @@
 const express = require("express");
 const app = express();
+const { Todo } = require("./models");
 const bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-const path = require("path");
-var csrf = require("tiny-csrf");
 app.use(bodyParser.json());
+const path = require("path");
 
-// Set EJS as view engine
 app.set("view engine", "ejs");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("shh! some secret string"));
-app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
-app.use(express.static(path.join(__dirname, "public")));
-
-const { Todo } = require("./models");
-
 app.get("/", async (request, response) => {
-  const overdue = await Todo.overdue();
-  const dueToday = await Todo.dueToday();
-  const dueLater = await Todo.dueLater();
-  const completed = await Todo.completed();
+  const allTodos = await Todo.getTodos();
   if (request.accepts("html")) {
     response.render("index", {
-      title: "Todo application",
-      overdue,
-      dueToday,
-      dueLater,
-      completed,
-      csrfToken: request.csrfToken(),
+      allTodos,
     });
   } else {
-    response.json({
-      overdue,
-      dueToday,
-      dueLater,
-      completed,
+    response.json("index", {
+      allTodos,
     });
   }
 });
 
-app.post("/todos", async (request, response) => {
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/todos", async (request, response) => {
+  console.log("Processing list of all Todos ...");
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
+  // Then, we have to respond with all Todos, like:
+  // response.send(todos)
   try {
-    await Todo.addTodo({
-      title: request.body.title,
-      dueDate: request.body.dueDate,
-    });
-    return response.redirect("/");
+    const todos = await Todo.findAll();
+    return response.json(todos);
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
   }
 });
 
-// PUT http://mytodoapp.com/todos/123/
-app.put("/todos/:id", async (request, response) => {
+app.get("/todos/:id", async function (request, response) {
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.post("/todos", async function (request, response) {
+  try {
+    const todo = await Todo.addTodo(request.body.title, request.body.dueDate);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async function (request, response) {
   const todo = await Todo.findByPk(request.params.id);
   try {
-    const updatedTodo = await todo.setCompletionStatus(request.body.completed);
+    const updatedTodo = await todo.markAsCompleted();
     return response.json(updatedTodo);
   } catch (error) {
     console.log(error);
@@ -66,12 +70,22 @@ app.put("/todos/:id", async (request, response) => {
   }
 });
 
-// eslint-disable-next-line no-unused-vars
-app.delete("/todos/:id", async (request, response) => {
+app.delete("/todos/:id", async function (request, response) {
+  console.log("We have to delete a Todo with ID: ", request.params.id);
+  // FILL IN YOUR CODE HERE
+
+  // First, we have to query our database to delete a Todo by ID.
+  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
+  // response.send(true)
   try {
-    await Todo.remove(request.params.id);
-    return response.json({ success: true });
+    const todo = await Todo.findByPk(request.params.id);
+    if (!todo) {
+      return response.send(false);
+    }
+    await todo.destroy();
+    return response.send(true);
   } catch (error) {
+    console.log(error);
     return response.status(422).json(error);
   }
 });
